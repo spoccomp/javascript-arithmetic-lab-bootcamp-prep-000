7d1c4a053e6a6d941f51ab0a42bdbc810463d13a
Richard Trapanese <richardtrapanese@gmail.com>
2018-03-02 03:04:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,12 +11,13 @@ function divide(a, b) {
   return a / b
 }
 function inc(n) {
-  return n++
+  return ++n
 }
 function dec(n) {
-  return n--
+  return --n
 }
 function makeInt(n) {
+
   parseInt(n)
   
 }
